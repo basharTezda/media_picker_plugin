41bf79718977951c2232ce885fca55dc2b484e65
Bashar Albashier <47083066+basharalbashier@users.noreply.github.com>
2025-05-14 12:59:14 +0300

    ..

--- lib/media_picker_plugin_method_channel.dart
+++ lib/media_picker_plugin_method_channel.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 
@@ -14,7 +16,85 @@ class MediaPicker {
 
   Future<void> sendEvent(Map<String, dynamic> event) async {
     await _methodChannel.invokeMethod('handleEvent', event);
+
+
   }
+
+   Future<void> _showMediaPicker(
+      {String? user,
+      ChatController? chatController,
+      RecorderNotifier? notifier,
+      ChatRoom? room,
+      TextEditingController? textEditingController,
+      context}) async {
+    _eventSubscription = _mediaPicker.mediaPickerEvents.listen((event) {
+      if (event['event'] == 'mediaSelected') {
+        selectedMediaPaths = List<String>.from(event['paths']);
+        controllerString = event['controller'];
+        method = event['method'];
+        textEditingController!.text = controllerString;
+        if (method == "send") {
+          state = state.copyWith(
+            mediaFileSelected: true,
+            displayFile: selectedMediaPaths.map((toElement) {
+              return File(toElement);
+            }).toList(),
+            chosenFile: File(selectedMediaPaths.first),
+            isVideoFile: true,
+          );
+          sendMediaFile(
+              user: user,
+              textEditingController: textEditingController,
+              chatController: chatController,
+              room: room,
+              notifier: notifier,
+              context: context);
+          textEditingController.clear();
+        } else {
+          _openEditor(
+            images: selectedMediaPaths,
+            user: user,
+            textEditingController: textEditingController,
+            chatController: chatController,
+            notifier: notifier,
+            room: room,
+            context: context,
+          );
+        }
+        // print("Media selected: ${event['paths']}");
+      } else if (event['event'] == 'pickerHidden') {
+        // print("Picker is hidden");
+      } else if (event['event'] == 'pickerReopened') {
+        // print("Picker is reopened");
+      }
+    });
+    await _mediaPicker.sendEvent({
+      "action": "showMediaPicker",
+      "text": textEditingController!.text,
+    });
+    // await _mediaPicker.sendEvent({
+    //   "action": "hideMediaPicker",
+    // });
+    // await _mediaPicker.sendEvent({
+    //   "action": "reopenMediaPicker",
+    // });
+    // try {
+    //   final Map<dynamic, dynamic> result = await platform
+    //       .invokeMethod('showMediaPicker', {'inputString': inputString});
+    //   // setState(() {
+    //   selectedMediaPaths = List<String>.from(result['paths']);
+    //   controllerString = result['controller'];
+    //   method = result['method'];
+    //   log(method,name: "method");
+
+    //   // });
+    // } on PlatformException catch (e) {
+    //   // setState(() {
+    //   // _selectedMediaPaths = "Failed to show media picker: '${e.message}'.";
+    //   // });
+    // }
+  }
+
 }
 
   final MediaPicker _mediaPicker = MediaPicker();
